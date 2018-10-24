09436890b6614eaa00580ec4c81dd4181360ce83
Magesh <tkmagesh77@gmail.com>
2018-10-24 15:35:34 +0530

    using promises

--- 06-async/asyncDemos.js
+++ 06-async/asyncDemos.js
@@ -14,22 +14,40 @@
 
 	window['addSyncClient'] = addSyncClient;
 
-	function addAsync(x,y){
+	function addAsync(x,y,callback){
 		console.log(`	[@Service] processing ${x} and ${y}`);
 		setTimeout(function(){
 			var result = x + y;
 			console.log(`	[@Service] returning result`);
-			return result;
+			callback(result);
 		}, 4000);
 	}
 
 	function addAsyncClient(x,y){
 		console.log(`[@Client] triggering addAsync`);
-		var result = addAsync(x,y);
-		console.log(`[@Client] result = ${result}`);
+		addAsync(x,y, function(result){
+			console.log(`[@Client] result = ${result}`);
+		});
 	}
 
 	window['addAsyncClient'] = addAsyncClient;
 
+	function addAsyncPromise(x,y){
+		console.log(`	[@Service] processing ${x} and ${y}`);
+
+		var promise = new Promise(function(resolveFn, rejectFn){
+
+			setTimeout(function(){
+				var result = x + y;
+				console.log(`	[@Service] returning result`);
+				resolveFn(result);
+			}, 4000);
+
+		});
+
+		return promise;
+	}
+
+	window['addAsyncPromise'] = addAsyncPromise;
 
 })();
